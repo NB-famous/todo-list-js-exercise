719bb8bf2f9cb19bca4ac2d1df52925f9f048e05
Nikko B <ncbadoles@gmail.com>
2020-07-31 18:22:50 +0000

    Step by step way of OOP, steps are commented out but the one not commented is that end result of what OPP can do

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
-// Arrays to keep track of each task's state
-const taskTitles = [];
-const taskComplete = [];
+
 
 // Create a new task by adding to the arrays
 // A new task will be created as incomplete
-function newTask(title) {
+
+//This is before code got refctored /////
+
+// Arrays to keep track of each task's state
+// const taskTitles = [];
+// const taskComplete = [];
+// const taskDescriptions = [];
+/* 
+  function newTask(title, description) {
   taskTitles.push(title);
+  taskDescriptions.push(description);
   taskComplete.push(false);
-}
+} 
 
 // Mark a task as complete by setting the task's status in the `taskComplete` array to `true`
 function completeTask(taskIndex) {
@@ -29,3 +36,102 @@ newTask("Do Laundry"); // task 1
 logTaskState(0); // Clean Cat Litter has not been completed
 completeTask(0);
 logTaskState(0); // Clean Cat Litter has been completed
+
+*/
+
+/////// We are Refactoring for step 2 ///
+/* 
+
+function newTask(title, description) {
+  const task = {
+    title: title,
+    description: description,
+    complete: false
+  };
+
+  return task;
+}
+
+// DRIVER CODE BELOW
+
+const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter box");
+const task2 = newTask("Do Laundry", "😨");
+const tasks = [task1, task2];
+
+// for now, let's just make sure we see our tasks
+console.log(tasks);//?
+
+*/
+
+
+/// Step 2  all task fucntions refactor Function 
+
+/* 
+function newTask(title, description) {
+  const task = {
+    title: title,
+    description: description,
+    complete: false
+  };
+  return task;
+}
+
+function logTaskState(task) {
+  console.log(`${task.title} has${task.complete ? " " : " not "}been completed`);
+}
+
+// marks the provided task as completed
+function completeTask(task) {
+  task.complete = true;
+}
+
+// DRIVER CODE BELOW
+
+const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter box");
+const task2 = newTask("Do Laundry", "😨");
+const tasks = [task1, task2];
+
+logTaskState(task1); // Clean Cat Litter has not been completed
+completeTask(task1);
+logTaskState(task1); // Clean Cat Litter has been completed
+
+console.log(tasks);
+*/
+
+/////////////////// Step 3 //////////////////////////////
+
+///Turn the 2 task functions into task object methods, /////
+/// Turn those fuction by adding them inside the newTask function called Methods////
+
+
+function newTask(title, description) {
+  const task = {
+    title: title,
+    description: description,
+    complete: false,
+
+    logState: function() {
+      console.log(`${this.title} has${this.complete ? " " : " not "}been completed`);
+    },
+
+    markCompleted: function() {
+      this.complete = true;
+    }
+  };
+  return task;
+}
+
+// DRIVER CODE CHANGES BELOW
+
+const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter box");
+const task2 = newTask("Do Laundry", "😨");
+const tasks = [task1, task2];
+
+task1.logState(); // Clean Cat Litter has not been completed
+task1.markCompleted();
+task1.logState(); // Clean Cat Litter has been completed
+
+
+
+////Note : OO bundles together state and logic into an object that can be represented as a single variable.
+//We applied this practice to an existing program and in doing so appreciate the improvement it gives us.
